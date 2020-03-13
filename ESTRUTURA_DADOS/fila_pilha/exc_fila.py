91145d20d51822e03f76b6e20ab06a05dc4dd9ce
rafaelwitter <rafawitt@hotmail.com>
2020-03-12 21:14:17 -0300

    Atualizado o arquivo fila.py

---
+++ ESTRUTURA_DADOS/fila_pilha/exc_fila.py
@@ -0,0 +1,5 @@
+from fila import Fila
+f1 = [1,5,12,4]
+fila = Fila(f1)
+fila.insere(2)
+fila.retira()
--- ESTRUTURA_DADOS/fila_pilha/fila.py
+++ ESTRUTURA_DADOS/fila_pilha/fila.py
@@ -1,3 +1,26 @@
 class Fila:
-    def __init__(self):
-        self.dados = []
+    def __init__(self, dados: []):
+        if type(dados) == list:
+            self.dados = dados
+            print(self.dados)
+        else:
+            return print("Adicione uma lista.")
+
+    def insere(self, elemento):
+        self.dados.append(elemento)
+        print(self.dados)
+    
+    def retira(self):
+        if self.vazia() is not True:
+            print(self.dados.pop(0))
+            print(self.dados)
+
+    def vazia(self):
+        if len(self.dados) == int(0):
+            print('A fila esta vazia.')
+            return True
+        else:
+            return False
+            
+
+    
